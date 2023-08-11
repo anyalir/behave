7005ddbbf3088942b3f27c6ad0f268701b387b46
Anya Livia Rudolph <anya.rudolph@clark.de>
2023-08-11 16:39:31 +0200

    feat: act dsl function

--- lib/scenario.ex
+++ lib/scenario.ex
@@ -12,24 +12,24 @@ defmodule Behave.Scenario do
 
   defmacro given(name, do: block) do
     name = Behave.string_to_function_name("given_#{name}")
+
     quote do
       def unquote(name)(scenario, _) do
         result = unquote(block)
         Behave.Scenario.__given__(scenario, result)
       end
     end
-    |> Behave.mdbg()
   end
 
   defmacro given(name, args, do: block) do
     name = Behave.string_to_function_name("given_#{name}")
+
     quote do
       def unquote(name)(scenario, unquote(args)) do
         result = unquote(block)
         Behave.Scenario.__given__(scenario, result)
       end
     end
-    |> Behave.mdbg()
   end
 
   def __given__(scenario, {key, datum}) when is_function(datum) do
@@ -46,6 +46,47 @@ defmodule Behave.Scenario do
 
   def __given__(_scenario, _) do
     raise "Given needs to return a {:key, value} tuple or :ignore for executing side effects."
+  end
+
+  defmacro act(name, do: block) do
+    name = Behave.string_to_function_name("act_#{name}")
+
+    quote do
+      def unquote(name)(scenario = %Behave.Scenario{data: d}, _) do
+        var!(data) = Map.new(d)
+        result = unquote(block)
+        Behave.Scenario.__act__(scenario, result)
+      end
+    end
+    |> Behave.mdbg()
+  end
+
+  defmacro act(name, args, do: block) do
+    name = Behave.string_to_function_name("act_#{name}")
+    quote do
+      def unquote(name)(scenario = %Behave.Scenario{data: d}, unquote(args)) do
+        var!(data) = Map.new(d)
+        result = unquote(block)
+        Behave.Scenario.__act__(scenario, result)
+      end
+    end
+  end
+
+  def __act__(scenario, {key, datum}) when is_function(datum) do
+    result = datum.(scenario.data[key])
+    put_in(scenario.results[key], result)
+  end
+
+  def __act__(scenario, {key, datum}) do
+    put_in(scenario.results[key], datum)
+  end
+
+  def __act__(_scenario, :ignore) do
+    :ok
+  end
+
+  def __act__(_scenario, _) do
+    raise "Act needs to return a {:key, value} tuple or :ignore for executing side effects."
   end
 
   def run(scenario = %__MODULE__{steps: steps}) do
--- test/support/test_steps.ex
+++ test/support/test_steps.ex
@@ -2,10 +2,6 @@ defmodule TestSteps do
     use Behave.Scenario
     import ExUnit.Assertions
 
-    # def given_coffee_machine(scenario, _args) do
-    #  put_in(scenario.data, [{:coffee_machine, CoffeeMachine.new()} | scenario.data])
-    # end
-
     given "coffee machine" do
       {:coffee_machine, CoffeeMachine.new()}
     end
@@ -17,9 +13,11 @@ defmodule TestSteps do
     given "it has coffee", cultivar: cultivar do
       {:coffee_machine, &CoffeeMachine.add_coffee(&1, cultivar)}
     end
-    def act_i_press_the_button(scenario, _args) do
-      put_in(scenario.results[:coffee], CoffeeMachine.brew(scenario.data[:coffee_machine]))
+
+    act "i press the button" do
+      {:coffee, CoffeeMachine.brew(data.coffee_machine)}
     end
+
     def check_it_makes_coffee(scenario, _args) do
       assert scenario.results[:coffee] != :disappointment
       scenario |> dbg
