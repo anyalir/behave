d2eead1462627bda75db46cc571c9d00235e8697
Anya Livia Rudolph <anya.rudolph@clark.de>
2023-08-11 18:52:12 +0200

    chore: make test neater

--- test/behave_explicit_test.exs
+++ test/behave_explicit_test.exs
@@ -5,12 +5,12 @@ defmodule BehaveExplicitTest do
 
   import TestSteps
   test "make coffee explicitly" do
-    scenario = Scenario.new()
-    scenario = Behave.__given__(scenario, &given_coffee_machine/2)
-    scenario = Behave.__given__(scenario, &given_it_has_water/2, amount: 259)
-    scenario = Behave.__given__(scenario, &given_it_has_coffee/2, cultivar: :crappy_robusta)
-    scenario = Behave.__act__(scenario, &act_i_press_the_button/2)
-    scenario = Behave.__check__(scenario, &check_it_makes_coffee/2)
-    Scenario.run(scenario)
+    Scenario.new()
+    |> Behave.__given__(&given_coffee_machine/2)
+    |> Behave.__given__(&given_it_has_water/2, amount: 259)
+    |> Behave.__given__(&given_it_has_coffee/2, cultivar: :crappy_robusta)
+    |> Behave.__act__(&act_i_press_the_button/2)
+    |> Behave.__check__(&check_it_makes_coffee/2)
+    |> Scenario.run()
   end
 end
