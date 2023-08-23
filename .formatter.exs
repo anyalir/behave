db0c5d48be1b87c52358ec45ba36b9bd6a9e360f
Eugene Yavaev <eugene.yavaev_external@clark.de>
2023-08-23 09:41:24 +0300

    feat: Add locals_without_parens to formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,9 @@
 # Used by "mix format"
+
+locals_without_parens = [given: :*, act: :*, check: :*]
+
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: locals_without_parens,
+  export: [locals_without_parens: locals_without_parens]
 ]
